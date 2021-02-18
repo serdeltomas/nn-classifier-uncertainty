23af9cdcd5cb22c8aee2acaf6f48cca9c5aa8441
setup425 <serdeltomas@gmail.com>
2021-02-18 23:14:40 +0100

    auroc calculations

--- project/evaluate_openmax.py
+++ project/evaluate_openmax.py
@@ -1,36 +1,41 @@
 import sklearn.metrics as slm
 import numpy as np
+import torch
 import matplotlib.pyplot as pyplot
+import time
 
+def get_TPTNFPFN_over_epsilon(sm_om, targets):
+    # is_openset = np.concatenate((np.full(10000, True), np.full(10000, False)))
+    tp, tn, fp, fn = [], [], [], []
+    for e in range(101):
+        epsilon = e/100.0
+        tp_, tn_, fp_, fn_ = 0, 0, 0, 0
+        for example in range(len(sm_om)):
+            if np.max(sm_om[example]) < epsilon:  #nok
+                if example < len(sm_om)/2.0:  #is actually OS
+                    tp_ += 1
+                elif example >= len(sm_om)/2.0:  #is actually CS
+                    fp_ += 1
+            else:  #ok
+                if example < len(sm_om)/2.0:  #is actually OS
+                    if np.argmax(sm_om[example]) == 10:  #guess OS
+                        tp_ += 1
+                    elif np.argmax(sm_om[example]) < 10:  #guess CS
+                        fn_ += 1
+                elif example >= len(sm_om)/2.0:  #is actually CS
+                    if np.argmax(sm_om[example]) == 10:  #guess OS
+                        fp_ += 1
+                    elif np.argmax(sm_om[example]) < 10:  #guess CS
+                        if np.argmax(sm_om[example]) == targets[example]:  #true
+                            tn_ += 1
+                        else:  #false
+                            fn_ += 1
+        assert tp_+tn_+fp_+fn_ == 20000
+        tp.append(tp_)
+        tn.append(tn_)
+        fn.append(fn_)
+        fp.append(fp_)
 
-def get_TPTNFPFN(sm_om, targets, epsilon):
-    tp, tn, fp, fn = 0, 0, 0, 0
-
-    for example in range(len(sm_om)):
-        if np.max(sm_om[example]) < epsilon:  #nok
-            if example < len(sm_om)/2.0:  #is actually OS
-                tn += 1
-            elif example >= len(sm_om)/2.0:  #is actually CS
-                fn += 1
-        else:  #ok
-            if example < len(sm_om)/2.0:  #is actually OS
-                if np.argmax(sm_om[example]) == 10:  #guess OS
-                    tn += 1
-                elif np.argmax(sm_om[example]) < 10:  #guess CS
-                    fp += 1
-            elif example >= len(sm_om)/2.0:  #is actually CS
-                if np.argmax(sm_om[example]) == 10:  #guess OS
-                    fn += 1
-                elif np.argmax(sm_om[example]) < 10:  #guess CS
-                    if np.argmax(sm_om[example]) == targets[example][0]:  #true
-                        tp += 1
-                    else:  #false
-                        fp += 1
-
-    assert tp+tn+fp+fn == len(sm_om)
-    # print(tp+tn+fp+fn, end="")
-    # print("/",end="")
-    # print(len(sm_om))
     return tp, tn, fp, fn
 # -------------------------------------------------------------------------------------------------------------------
 
@@ -81,78 +86,117 @@ def evaluate(sm_om, targets, epsilon):
 # -------------------------------------------------------------------------------------------------------------------
 
 
+def compute_auroc_old(om_sm, targets):
+    tp, tn, fp, fn = get_TPTNFPFN_over_epsilon(om_sm,targets)
+    fpr, tpr = [], []
+    for i in range(101):
+        if not fp[i] and not tn[i]:
+            fpr.append(0)
+        else:
+            fpr.append(fp[i]/(fp[i] + tn[i]))
+        if not tp[i] and not fn[i]:
+            tpr.append(0)
+        else:
+            tpr.append(tp[i] / (tp[i] + fn[i]))
+
+    auroc = slm.auc(fpr, tpr)
+    return auroc
+# -------------------------------------------------------------------------------------------------------------------
+
+
+def compute_auroc(topk, is_positive):
+    fpr, tpr = [], []
+    is_rejected = is_rejected_all(topk)
+
+    for epsilon in range(101):
+        tp, tn, fp, fn = 0, 0, 0, 0
+        for is_pos, is_rej in zip(is_positive, is_rejected[epsilon]):
+            if is_rej and is_pos:
+                tp += 1
+            elif not is_rej and not is_pos:
+                tn += 1
+            elif is_rej and not is_pos:
+                fp += 1
+            elif not is_rej and is_pos:
+                fn += 1
+
+        assert tp+tn+fp+fn == 20000
+
+        if not fp and not tn:
+            fpr.append(0)
+        else:
+            fpr.append(fp / (fp + tn))
+        if not tp and not fn:
+            tpr.append(0)
+        else:
+            tpr.append(tp / (tp + fn))
+
+    auroc = slm.auc(fpr, tpr)
+    return auroc, is_rejected
+# -------------------------------------------------------------------------------------------------------------------
+
+
+def is_rejected_one(position, value, epsilon):
+    return position == 10 or value < epsilon
+# -------------------------------------------------------------------------------------------------------------------
+
+
+def is_rejected_over_epsilon(position, value):
+    is_rej = []
+    for epsilon in range(101):
+        is_rej.append(is_rejected_one(position, value, epsilon/100.0))
+    return np.asarray(is_rej)
+# -------------------------------------------------------------------------------------------------------------------
+
+
+def is_rejected_all(topk):
+    is_rej_all = []
+    for topk_val, topk_pos in zip(topk[0], topk[1]):
+        is_rej_all.append(is_rejected_over_epsilon(topk_pos, topk_val))
+    is_rej_all = np.asarray(is_rej_all)
+    return is_rej_all.T
+# -------------------------------------------------------------------------------------------------------------------
+
+
+def is_positive_all(targets):
+    is_pos_all = []
+    for targ in targets:
+        if targ == 10:
+            is_pos_all.append(True)
+        else:
+            is_pos_all.append(False)
+    return np.asarray(is_pos_all)
+# -------------------------------------------------------------------------------------------------------------------
+
+
 def main_grid_search():
-    targets = np.concatenate((np.load("preprocessing/c100_test_targ.npy", allow_pickle=True),
-                              np.load("preprocessing/c10_test_targ.npy", allow_pickle=True)), axis=0)
-    openmax_all, softmax_all = [], []
+    x, y = np.hsplit(np.load("preprocessing/c10_test_targ.npy", allow_pickle=True), 2)
+    targets = np.concatenate((np.full(10000, 10), x.flatten()))
+    is_positive = np.concatenate((np.full(10000, True), np.full(10000, False)))
+    auroc_sm_all = np.zeros((29, 8))
+    auroc_om_all = np.zeros((29, 8))
 
-    openmax_best = dict(sum_eval=-1, eval=-1, alpha=-1, tail=-1, epsilon=-1)
-    softmax_best = dict(sum_eval=-1, eval=-1, alpha=-1, tail=-1, epsilon=-1)
-    both_best = dict(sum_eval=-1, eval_o=-1, eval_s=-1, alpha=-1, tail=-1, epsilon=-1)
-    openmax_best_auroc = dict(auroc=-1, alpha=-1, tail=-1)
-    softmax_best_auroc = dict(auroc=-1, alpha=-1, tail=-1)
-    both_best_auroc = dict(auroc=-1, alpha=-1, tail=-1)
-
-    for i in range(int(len(targets) / 2)):
-        targets[i][0] = 10
-
-    for tail_size in range(1, 31):
+    counter = 0
+    for tail_size in range(2, 31):
         for alpha in range(1, 9):
-            auroc_om, aurooc_sm, auroc_both = 0, 0, 0
-            fpr_om, fpr_sm, tpr_om, tpr_sm = [], [], [], []
-            openmax = np.load("testing/om_a%d_t%d.npy" %(alpha, tail_size), allow_pickle=True)
-            softmax = np.load("testing/sm_a%d_t%d.npy" %(alpha, tail_size), allow_pickle=True)
-            for epsilon in range(0, 101):
-                sm_eval = evaluate(np.asarray(softmax), targets, epsilon / 100.0)
-                om_eval = evaluate(np.asarray(openmax), targets, epsilon / 100.0)
-
-                openmax_ = dict(sum_eval=-1, eval=-1, alpha=-1, tail=-1, epsilon=-1)
-                softmax_ = dict(sum_eval=-1, eval=-1, alpha=-1, tail=-1, epsilon=-1)
-                softmax_['sum_eval'], softmax_['eval'] = sm_eval['sum_eval'], sm_eval
-                softmax_['alpha'], softmax_['tail'], softmax_['epsilon'] = alpha, tail_size, epsilon
-                openmax_['sum_eval'], openmax_['eval'] = om_eval['sum_eval'], om_eval
-                openmax_['alpha'], openmax_['tail'], openmax_['epsilon'] = alpha, tail_size, epsilon
-                openmax_all.append(openmax_)
-                softmax_all.append(softmax_)
-                fpr_om.append(om_eval['fpr'])
-                fpr_sm.append(sm_eval['fpr'])
-                tpr_om.append(om_eval['recall'])
-                tpr_sm.append(sm_eval['recall'])
-                if sm_eval['sum_eval'] > softmax_best['sum_eval']:
-                    softmax_best['sum_eval'], softmax_best['eval'] = sm_eval['sum_eval'], sm_eval
-                    softmax_best['alpha'], softmax_best['tail'], softmax_best['epsilon'] = alpha, tail_size, epsilon
-                if om_eval['sum_eval'] > openmax_best['sum_eval']:
-                    openmax_best['sum_eval'], openmax_best['eval'] = om_eval['sum_eval'], om_eval
-                    openmax_best['alpha'], openmax_best['tail'], openmax_best['epsilon'] = alpha, tail_size, epsilon
-                if (om_eval['sum_eval'] + sm_eval['sum_eval'])/2.0 > both_best['sum_eval']:
-                    both_best['sum_eval'] = (om_eval['sum_eval'] + sm_eval['sum_eval'])/2.0
-                    both_best['eval_o'], both_best['eval_s'] = om_eval, sm_eval
-                    both_best['alpha'], both_best['tail'], both_best['epsilon'] = alpha, tail_size, epsilon
-            auroc_om = slm.auc(fpr_om, tpr_om)
-            auroc_sm = slm.auc(fpr_sm, tpr_sm)
-            auroc_both = (auroc_sm + auroc_om) / 2.0
-            # pyplot.plot(fpr_om, tpr_om)
-            # pyplot.plot(fpr_sm, tpr_sm)
-            if auroc_sm > softmax_best_auroc['auroc']:
-                softmax_best_auroc['auroc'] = auroc_sm
-                softmax_best_auroc['alpha'], softmax_best_auroc['tail'] = alpha, tail_size
-            if auroc_om > openmax_best_auroc['auroc']:
-                openmax_best_auroc['auroc'] = auroc_om
-                openmax_best_auroc['alpha'], openmax_best_auroc['tail'] = alpha, tail_size
-            if auroc_both > both_best_auroc['auroc']:
-                both_best_auroc['auroc'] = auroc_both
-                both_best_auroc['alpha'], both_best_auroc['tail'] = alpha, tail_size
-
-    openmax_all = np.asarray(openmax_all)
-    softmax_all = np.asarray(softmax_all)
-    np.save("results/openmax_test_results_a1-8_t1-30_e0-100.npy", openmax_all)
-    np.save("results/softmax_test_results_a1-8_t1-30_e0-100.npy", softmax_all)
-    print(openmax_best)
-    print(softmax_best)
-    print(both_best)
-    print(openmax_best_auroc)
-    print(softmax_best_auroc)
-    print(both_best_auroc)
+            start_time = time.time()
+            openmax = np.load("testing/om_a%d_t%d.npy" % (alpha, tail_size), allow_pickle=True)
+            softmax = np.load("testing/sm_a%d_t%d.npy" % (alpha, tail_size), allow_pickle=True)
+            openmax_topk = torch.topk(torch.from_numpy(openmax), k=1, dim=1)
+            softmax_topk = torch.topk(torch.from_numpy(softmax), k=1, dim=1)
+            auroc_om_all[tail_size - 2][alpha - 1], is_rej_om = compute_auroc(openmax_topk, is_positive)
+            auroc_sm_all[tail_size - 2][alpha - 1], is_rej_sm = compute_auroc(softmax_topk, is_positive)
+            np.save("testing/rejected/om_rej_a%d_t%d.npy" % (alpha, tail_size), is_rej_om)
+            np.save("testing/rejected/sm_rej_a%d_t%d.npy" % (alpha, tail_size), is_rej_sm)
+            # openmax_same = (np.asarray(openmax_topk[1]).flatten() == targets)
+            # softmax_same = (np.asarray(softmax_topk[1]).flatten() == targets)
+            # auroc_om_all[targets-2][alpha-1] = compute_auroc_old(openmax, targets)
+            # auroc_sm_all[targets-2][alpha-1] = compute_auroc_old(softmax, targets)
+            counter += 1
+            print("%s/232 " % (str(counter).zfill(3)), end="")
+            print("--- %s seconds ---" % (time.time() - start_time))
+    print(auroc_om_all)
+    print(auroc_sm_all)
     return 0
 # -------------------------------------------------------------------------------------------------------------------
 
