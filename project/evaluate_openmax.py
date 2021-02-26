2936542e5d5e180bfd8b1190aba5ced0b54d66fc
setup425 <serdeltomas@gmail.com>
2021-02-26 12:52:10 +0100

    added precision and recall

--- project/evaluate_openmax.py
+++ project/evaluate_openmax.py
@@ -4,105 +4,6 @@ import torch
 import matplotlib.pyplot as pyplot
 import time
 
-def get_TPTNFPFN_over_epsilon(sm_om, targets):
-    # is_openset = np.concatenate((np.full(10000, True), np.full(10000, False)))
-    tp, tn, fp, fn = [], [], [], []
-    for e in range(101):
-        epsilon = e/100.0
-        tp_, tn_, fp_, fn_ = 0, 0, 0, 0
-        for example in range(len(sm_om)):
-            if np.max(sm_om[example]) < epsilon:  #nok
-                if example < len(sm_om)/2.0:  #is actually OS
-                    tp_ += 1
-                elif example >= len(sm_om)/2.0:  #is actually CS
-                    fp_ += 1
-            else:  #ok
-                if example < len(sm_om)/2.0:  #is actually OS
-                    if np.argmax(sm_om[example]) == 10:  #guess OS
-                        tp_ += 1
-                    elif np.argmax(sm_om[example]) < 10:  #guess CS
-                        fn_ += 1
-                elif example >= len(sm_om)/2.0:  #is actually CS
-                    if np.argmax(sm_om[example]) == 10:  #guess OS
-                        fp_ += 1
-                    elif np.argmax(sm_om[example]) < 10:  #guess CS
-                        if np.argmax(sm_om[example]) == targets[example]:  #true
-                            tn_ += 1
-                        else:  #false
-                            fn_ += 1
-        assert tp_+tn_+fp_+fn_ == 20000
-        tp.append(tp_)
-        tn.append(tn_)
-        fn.append(fn_)
-        fp.append(fp_)
-
-    return tp, tn, fp, fn
-# -------------------------------------------------------------------------------------------------------------------
-
-
-def evaluate(sm_om, targets, epsilon):
-    sum_eval = 0
-    eval_om_sm = dict(sum_eval=-1, f_measure=-1, precision=-1, recall=-1, fpr=-1, fpr95=-1, false_neg_rate=-1,
-                classification_accuracy=-1, target_success_rate=-1, tp=-1, tn=-1, fp=-1, fn=-1)
-
-    targets_sm_om = 0
-    for cs_example in range(int(len(targets) / 2.0), len(targets)):
-        if targets[cs_example][0] == np.argmax(sm_om[cs_example]) and sm_om[cs_example][np.argmax(sm_om[cs_example])] >= epsilon:
-            targets_sm_om += 1
-    eval_om_sm['classification_accuracy'] = targets_sm_om/(len(targets)/2.0)
-    eval_om_sm['tp'], eval_om_sm['tn'], eval_om_sm['fp'], eval_om_sm['fn'] = get_TPTNFPFN(sm_om, targets, epsilon)
-    # fn/(fn+tp)
-    if not eval_om_sm['fn'] and not eval_om_sm['tp']:
-        eval_om_sm['false_neg_rate'] = 0
-    else:
-        eval_om_sm['false_neg_rate'] = eval_om_sm['fn'] / (eval_om_sm['fn'] + eval_om_sm['tp'])
-    # fp/(fp+tn)
-    if not eval_om_sm['fp'] and not eval_om_sm['tn']:
-        eval_om_sm['fpr'] = 0
-    else:
-        eval_om_sm['fpr'] = eval_om_sm['fp'] / (eval_om_sm['fp'] + eval_om_sm['tn'])
-    # tp/(tp+fp)
-    if not eval_om_sm['tp'] and not eval_om_sm['fp']:
-        eval_om_sm['precision'] = 0
-    else:
-        eval_om_sm['precision'] = eval_om_sm['tp'] / (eval_om_sm['tp'] + eval_om_sm['fp'])
-    # tp/(tp+fn)
-    if not eval_om_sm['tp'] and not eval_om_sm['fn']:
-        eval_om_sm['recall'] = 0
-    else:
-        eval_om_sm['recall'] = eval_om_sm['tp'] / (eval_om_sm['tp'] + eval_om_sm['fn'])
-    # recall=1-fnr
-    assert 1 == eval_om_sm['recall'] + eval_om_sm['false_neg_rate']
-    # 2*precision*recall/(precision+recall)
-    if not eval_om_sm['precision'] and not eval_om_sm['recall']:
-        eval_om_sm['f_measure'] = 0
-    else:
-        eval_om_sm['f_measure'] = 2 * eval_om_sm['precision'] * eval_om_sm['recall'] / (eval_om_sm['precision'] + eval_om_sm['recall'])
-
-    eval_om_sm['sum_eval'] = (eval_om_sm['f_measure'] + eval_om_sm['classification_accuracy'] + (1 - eval_om_sm['false_neg_rate'])) * (100.0/3.0)
-
-    print("", end="")
-    return eval_om_sm  #sum_eval is 0-100 with 100 being the best
-# -------------------------------------------------------------------------------------------------------------------
-
-
-def compute_auroc_old(om_sm, targets):
-    tp, tn, fp, fn = get_TPTNFPFN_over_epsilon(om_sm,targets)
-    fpr, tpr = [], []
-    for i in range(101):
-        if not fp[i] and not tn[i]:
-            fpr.append(0)
-        else:
-            fpr.append(fp[i]/(fp[i] + tn[i]))
-        if not tp[i] and not fn[i]:
-            tpr.append(0)
-        else:
-            tpr.append(tp[i] / (tp[i] + fn[i]))
-
-    auroc = slm.auc(fpr, tpr)
-    return auroc
-# -------------------------------------------------------------------------------------------------------------------
-
 
 def compute_auroc(is_positive, is_rejected):
     # computes auroc for one combination of tail and alpha
@@ -168,16 +69,35 @@ def compute_aupr(is_positive, is_rejected):
 # -------------------------------------------------------------------------------------------------------------------
 
 
-def is_rejected_one(position, value, epsilon):
-    return position == 10 or value < epsilon
-# -------------------------------------------------------------------------------------------------------------------
 
+def compute_precision_recall(is_positive, is_rejected):
+    # computes precision and recall for one combination of tail and alpha over epsilon
+    precision, recall = [], []  # tpr=recall
 
-def is_rejected_over_epsilon(position, value):
-    is_rej = []
     for epsilon in range(101):
-        is_rej.append((position == 10 or value < epsilon/100.0))
-    return np.asarray(is_rej)
+        tp, tn, fp, fn = 0, 0, 0, 0
+        for is_pos, is_rej in zip(is_positive, is_rejected[epsilon]):
+            if is_rej and is_pos:
+                tp += 1
+            elif not is_rej and not is_pos:
+                tn += 1
+            elif is_rej and not is_pos:
+                fp += 1
+            elif not is_rej and is_pos:
+                fn += 1
+
+        assert tp+tn+fp+fn == 20000
+
+        if not tp and not fp:
+            precision.append(0)
+        else:
+            precision.append(tp / (tp + fp))
+        if not tp and not fn:
+            recall.append(0)
+        else:
+            recall.append(tp / (tp + fn))
+
+    return precision, recall
 # -------------------------------------------------------------------------------------------------------------------
 
 
@@ -193,27 +113,16 @@ def is_rejected_all(topk):
 # -------------------------------------------------------------------------------------------------------------------
 
 
-def is_positive_all(targets):
-    is_pos_all = []
-    for targ in targets:
-        if targ == 10:
-            is_pos_all.append(True)
-        else:
-            is_pos_all.append(False)
-    return np.asarray(is_pos_all)
-# -------------------------------------------------------------------------------------------------------------------
-
-
 def classification_accuracy(targets, predictions):
     # sm = np.load("testing/sm.npy", allow_pickle=True)
     # om = np.load("testing/om_a%d_t%d.npy" % (alpha,tail), allow_pickle=True)
 
     ca = 0
-    for i in range(20000):
+    for i in range(10000, 20000):
         if targets[i] == np.argmax(predictions[i]):
             ca += 1
 
-    ca = ca/20000.0
+    ca = ca/10000.0
     return ca
 # -------------------------------------------------------------------------------------------------------------------
 
@@ -223,60 +132,26 @@ def classification_accuracy_epsilon(targets, predictions, epsilon):
     # om = np.load("testing/om_a%d_t%d.npy" % (alpha,tail), allow_pickle=True)
 
     ca = 0
-    for i in range(20000):
+    for i in range(10000, 20000):
         argm = np.argmax(predictions[i])
         max = predictions[i][argm]
-        if max >= epsilon and targets[i] == argm:
-            ca += 1
-        elif max < epsilon and targets[i] == 10:
+        if max >= epsilon and targets[i] == argm:  # zamietni ak epsilon je ostro väčšie
             ca += 1
 
-
-    ca = ca/20000.0
+    ca = ca/10000.0
     return ca
-# -------------------------------------------------------------------------------------------------------------------
-
-
-def find_recall_95(is_positive, is_rejected):
-    fpr, tpr = [], []  # tpr=recall
-
-    for epsilon in range(101):
-        tp, tn, fp, fn = 0, 0, 0, 0
-        for is_pos, is_rej in zip(is_positive, is_rejected[epsilon]):
-            if is_rej and is_pos:
-                tp += 1
-            elif not is_rej and not is_pos:
-                tn += 1
-            elif is_rej and not is_pos:
-                fp += 1
-            elif not is_rej and is_pos:
-                fn += 1
-
-        assert tp + tn + fp + fn == 20000
-
-        if not fp and not tn:
-            fpr.append(0)
-        else:
-            fpr.append(fp / (fp + tn))
-        if not tp and not fn:
-            tpr.append(0)
-        else:
-            tpr.append(tp / (tp + fn))
-
-    auroc = slm.auc(fpr, tpr)
-    return auroc
 # -------------------------------------------------------------------------------------------------------------------
 
 
 def save_rejected():
     for tail_size in range(2, 22):
         for alpha in range(1, 7):
-            start_time = time.time()
+            # start_time = time.time()
             om = np.load("testing/om_a%d_t%d.npy" % (alpha, tail_size), allow_pickle=True)
             om_topk = torch.topk(torch.from_numpy(om), k=1, dim=1)
             is_rej_om = is_rejected_all(om_topk)
-            np.save("testing/rejected/om_rej_a%d_t%d.npy" % (alpha, tail_size), is_rej_om)
-            print("--- %s seconds ---" % (time.time() - start_time))
+            # np.save("testing/rejected/om_rej_a%d_t%d.npy" % (alpha, tail_size), is_rej_om)
+            # print("--- %s seconds ---" % (time.time() - start_time))
     return True
 # -------------------------------------------------------------------------------------------------------------------
 
@@ -323,6 +198,36 @@ def grid_aupr():
 # -------------------------------------------------------------------------------------------------------------------
 
 
+def grid_precision_recall():
+    is_positive = np.concatenate((np.full(10000, True), np.full(10000, False)))
+    sm_rej = np.load("testing/rejected/sm_rej.npy", allow_pickle=True)
+    pr_om_all, re_om_all = [], []
+    # pr_sm_all, re_sm_all = 0, 0
+    alpha = [3, 2, 1]
+    tail = [3, 7, 2]
+
+    pr_sm_all, re_sm_all = compute_precision_recall(is_positive, sm_rej)
+    for al_ta in range(3):
+        om_rej = np.load("testing/rejected/om_rej_a%d_t%d.npy" % (alpha[al_ta], tail[al_ta]), allow_pickle=True)
+        pr_om, re_om = compute_precision_recall(is_positive, om_rej)
+        pr_om_all.append(pr_om)
+        re_om_all.append(re_om)
+
+    # numpy_array = np.array(pr_om_all)
+    # transpose = numpy_array.T
+    # pr_om_all = transpose.tolist()
+    # numpy_array = np.array(re_om_all)
+    # transpose = numpy_array.T
+    # re_om_all = transpose.tolist()
+
+    print(pr_om_all)
+    print(re_om_all)
+    print(pr_sm_all)
+    print(re_sm_all)
+    return 0
+# -------------------------------------------------------------------------------------------------------------------
+
+
 def grid_ca():
     x, y = np.hsplit(np.load("preprocessing/c10_test_targ.npy", allow_pickle=True), 2)
     targets = np.concatenate((np.full(10000, 10), x.flatten()))
@@ -348,22 +253,30 @@ def grid_ca_eps():
     targets = np.concatenate((np.full(10000, 10), x.flatten()))
     sm = np.load("testing/sm.npy", allow_pickle=True)
     ca_om_all, ca_sm_all = [], []
-    for eps in range(0, 101, 10):
-        ca_om = np.zeros((29, 8))
+    alpha = [3, 2, 1]
+    tail = [3, 7, 2]
+    for eps in range(0, 101):
+        ca_om = np.zeros(3)
         ca_sm = classification_accuracy_epsilon(targets, sm, eps/100.0)
         # counter = 0
-        for tail_size in range(2, 31):
-            for alpha in range(1, 9):
-                om = np.load("testing/om_a%d_t%d.npy" % (alpha,tail_size), allow_pickle=True)
-                ca_om[tail_size - 2][alpha - 1] = classification_accuracy_epsilon(targets, om, eps/100.0)
-                # counter += 1
-                # print("%s " % (str(counter).zfill(3)), end="")
-                # print("--- %s seconds ---" % (time.time() - start_time))
+        for al_ta in range(3):
+            om = np.load("testing/om_a%d_t%d.npy" % (alpha[al_ta], tail[al_ta]), allow_pickle=True)
+            ca_om[al_ta] = classification_accuracy_epsilon(targets, om, eps/100.0)
+            # counter += 1
+            # print("%s " % (str(counter).zfill(3)), end="")
+            # print("--- %s seconds ---" % (time.time() - start_time))
         ca_sm_all.append(ca_sm)
         ca_om_all.append(ca_om)
+    # ca_om_all = np.asarray(ca_om_all)
+    # ca_sm_all = np.asarray(ca_sm_all)
+
+    numpy_array = np.array(ca_om_all)
+    transpose = numpy_array.T
+    ca_om_all = transpose.tolist()
+
     print(ca_om_all)
     print(ca_sm_all)
-    return ca_om, ca_sm
+    return ca_om_all, ca_sm_all
 # -------------------------------------------------------------------------------------------------------------------
 
 
@@ -371,5 +284,6 @@ if __name__ == "__main__":
     # grid_auroc()
     # grid_aupr()
     # save_rejected()
-    grid_ca()
     # grid_ca_eps()
+    # grid_ca_eps()
+    grid_precision_recall()
